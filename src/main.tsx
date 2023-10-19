c0fa575cd639d158e0f582c55ff97899d4c875f2
Richard dos Santos Otto <shoonkey.dev@gmail.com>
2023-10-18 23:30:11 -0300

    chore: removed setup file abstraction

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
-import setupApp from "./setup";
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-setupApp({ containerId: "root" });
+import App from "./App";
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
